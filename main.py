b4fdffd5d49e277f4b23bd16db4b162c99df27e4
WunderMrow <112693429+WunderMrow@users.noreply.github.com>
2023-09-07 09:41:30 +0900

    Add Initial Programs
    Replit ———> Git  —X—> Github
    Replit ———> ZIP ———> GitHub

---
+++ main.py
@@ -0,0 +1,227 @@
+
+import random
+import os
+import time
+
+class override(Exception):
+ "turn the output into specified changed item"
+pass
+
+input("press enter to continue...")
+
+os.system("clear")
+
+A = input("item A ")
+B = input("item B ")
+C = input("item C ")
+D = input("item D ")
+E = input("item E ")
+F = input("item F ")
+G = input("item G ")
+H = input("item H ")
+I = input("item I ")
+
+Cat=[A, B, C, D, E, F, G, H, I]
+CatGatcha=(random.choice(Cat))
+
+
+
+print("")
+print("")
+print(".      □        ")
+print("∆______________∆")
+print("|              |")
+print("|              |")
+print("|    U     U   |")
+print("|       W      |")
+print("|              |")
+print("|              |")
+print("|              |")
+print("----------------")
+print("|      0       |")
+print("|    -----     |")
+print("|   | | | |    |")
+print("|   |_/_\_|    |")
+print("----------------")
+print("")
+print("")
+time.sleep(1)
+os.system("clear")
+
+print("")
+print("∆______□_______∆")
+print("|              |")
+print("|              |")
+print("|    U     U   |")
+print("|       W      |")
+print("|              |")
+print("|              |")
+print("|              |")
+print("----------------")
+print("|      0       |")
+print("|    -----     |")
+print("|   | | | |    |")
+print("|   |_/_\_|    |")
+print("----------------")
+print("")
+print("")
+time.sleep(1)
+os.system("clear")
+
+print("")
+print("∆______________∆")
+print("|              |")
+print("|              |")
+print("|    U     U   |")
+print("|       W      |")
+print("|              |")
+print("|              |")
+print("|              |")
+print("----------------")
+print("|      0       |")
+print("|    -----     |")
+print("|   | | | |    |")
+print("|   |_/_\_|    |")
+print("----------------")
+print("")
+print("")
+time.sleep(1)
+os.system("clear")
+
+print("")
+print("∆______________∆")
+print("|              |")
+print("|              |")
+print("|    O     O   |")
+print("|       W      |")
+print("|              |")
+print("|              |")
+print("|              |")
+print("----------------")
+print("|      0       |")
+print("|    -----     |")
+print("|   | | | |    |")
+print("|   |_/_\_|    |")
+print("----------------")
+print("")
+print("")
+time.sleep(1)
+os.system("clear")
+
+print("")
+print("∆______________∆")
+print("|              |")
+print("|              |")
+print("|    >     <   |")
+print("|       W      |")
+print("|              |")
+print("|              |")
+print("|              |")
+print("----------------")
+print("|      0       |")
+print("|    -----     |")
+print("|   | | | |    |")
+print("|   |_/○\_|    |")
+print("----------------")
+print("")
+print("")
+time.sleep(1)
+os.system("clear")
+
+Overide= [B, D, F, H]
+OverRide=(random.choice(Overide))
+
+number=1
+try:
+    input_num = int(input("Ready? if yes, press -1: "))
+    if input_num < number:
+        raise override
+    else:
+        pass
+except override: pass 
+
+
+
+print(" ________")
+print("/        \ ")
+print("|_________|")
+print("|         |")
+print("\         /")
+print(" ‾‾‾‾‾‾‾‾‾")
+time.sleep(0.5)
+os.system("clear")
+
+print(" ________")
+print("/        \ ")
+print("|_________|")
+print("___________")
+print("|         |")
+print("\         /")
+print(" ‾‾‾‾‾‾‾‾‾")
+time.sleep(0.5)
+os.system("clear")
+
+print(" ________")
+print("/        \ ")
+print("|_________|")
+print("")
+print("___________")
+print("|         |")
+print("\         /")
+print(" ‾‾‾‾‾‾‾‾‾")
+time.sleep(0.5)
+os.system("clear")
+
+print(" ________")
+print("/        \ ")
+print("|_________|")
+print("   |   |   ")
+print(".  |   |   ")
+print("___|___|___")
+print("|         |")
+print("\         /")
+print(" ‾‾‾‾‾‾‾‾‾")
+time.sleep(0.5)
+os.system("clear")
+
+print(" ________")
+print("|        |")
+print("|        |")
+print("|        |")
+print("|        |")
+print("|        |")
+print("|        |")
+print(" ‾‾‾‾‾‾‾‾‾")
+time.sleep(0.5)
+os.system("clear")
+
+print(" ________")
+print(" \       \ ")
+print(". \       \ ")
+print("   \       \  ")
+print("    \       \  ")
+print("     \       \ ")
+print("      \       \ ")
+print("       ‾‾‾‾‾‾‾‾‾ ")
+time.sleep(0.5)
+os.system("clear")
+try:
+  if input_num < number:
+    raise override
+ 
+    print("The CatGatcha spitted out...")
+    print("______________________")
+    print("|                     |")
+    print("|the results  "+str(CatGatcha)) 
+    print("|                     |") 
+    print("|_____________________|")
+
+except override:
+ os.system("clear")
+ print("The Catgatcha spitted out...")
+ print("______________________")
+ print("|                     |")
+ print("|the results  "+str(OverRide))
+ print("|                     |")
+ print("|______________________|") 
+pass
